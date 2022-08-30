f54cf5d29f556613702caf8eac9c5f8a94136d71
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-08-30 15:27:42 +0200

    Log the data

---
+++ esercizi/dist/05-Exercise.js
@@ -0,0 +1,38 @@
+// Challenge 1:
+var surprisingFact = "The bumblebee bat is the world's smallest mammal";
+console.log("Surprising fact: ", surprisingFact);
+console.error("surprisingFactError:", surprisingFact);
+//Challenge 2 :
+var familyTree = [
+    {
+        name: "Person 1",
+        children: [
+            {
+                name: "Person 2",
+                children: [
+                    {
+                        name: "Person 3",
+                        children: [
+                            {
+                                name: "Person 4"
+                            },
+                        ]
+                    },
+                ]
+            },
+        ]
+    },
+];
+console.log(JSON.stringify(familyTree, null, 2));
+console.dir(familyTree, { depth: null });
+//Challenge 3:
+function importantTask() {
+    console.count("importantTaskCount:");
+}
+importantTask();
+importantTask();
+importantTask();
+importantTask();
+console.countReset("importantTaskCount:");
+importantTask();
+importantTask();
