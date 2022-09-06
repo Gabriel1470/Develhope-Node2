1418df41254093bd856d24f1fe0f19e02aa1661d
Gabriel Bogdan <99339866+Gabriel1470@users.noreply.github.com>
2022-09-06 19:01:47 +0200

    Prepare your application for deployment

--- esercizi/Implement authentication/src/app.js
+++ esercizi/Implement authentication/src/app.js
@@ -7,19 +7,25 @@ const validationErrorMiddleware =
 const initSessionMiddleware = require('./lib/middleware/session');
 const { passport } = require('./lib/middleware/passport');
 const authRoutes = require('./routes/auth');
+const {
+  notFoundMiddleware,
+  initErrorMiddleware,
+} = require('./lib/middleware/error');
 
-app.use(initSessionMiddleware());
+app.use(initSessionMiddleware(app.get('env')));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.json());
-
 app.use(initCorsMiddleware());
-
 app.use('/planets', planetsRoutes);
 app.use('/auth', authRoutes);
 
+app.use(notFoundMiddleware);
+
 app.use(validationErrorMiddleware);
+
+app.use(initErrorMiddleware(app.get('env')));
 
 module.exports = app;
